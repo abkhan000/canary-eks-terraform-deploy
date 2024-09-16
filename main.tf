86dfdbf7afbdab7e4f7671436e28510c5bdb0b1d
abrarkhan <123900280+abkhan000@users.noreply.github.com>
2024-09-16 18:44:21 +0530

    Update main.tf (AZs updated to us-east-2 )

--- main.tf
+++ main.tf
@@ -2,39 +2,33 @@ provider "aws" {
   region = "us-east-2"
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+  name    = "my-vpc"
+  cidr    = "10.0.0.0/16"
+  azs     = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
+}
+
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "19.0.0"
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.27"
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
   eks_managed_node_groups = {
     main = {
       min_size     = 2
       max_size     = 3
       desired_size = 2
-
       instance_type = "t3.small"
     }
   }
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-  single_nat_gateway = true
 }
 
 output "cluster_endpoint" {
