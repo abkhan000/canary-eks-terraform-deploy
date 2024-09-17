2c6382feebc28c39fe5ee0ec53931158d2750e26
abrarkhan <123900280+abkhan000@users.noreply.github.com>
2024-09-17 19:38:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,42 +1,81 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "5.0.0"
-  name    = "my-vpc"
-  cidr    = "10.0.0.0/16"
-  azs     = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-  enable_nat_gateway = true
-  single_nat_gateway = true
-}
-
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "19.0.0"
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.27"
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.private_subnets
-  eks_managed_node_groups = {
-    main = {
-      min_size     = 2
-      max_size     = 3
-      desired_size = 2
-      instance_type = "t3.small"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.10.0"
     }
   }
 }
 
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.eks.cluster_endpoint
+provider "aws" {
+  region     = "ap-south-1"
 }
 
-output "cluster_name" {
-  description = "Kubernetes Cluster Name"
-  value       = module.eks.cluster_name
+data "aws_eks_cluster" "my_cluster" {
+  name = "my-eks-cluster"
 }
+
+data "aws_eks_cluster_auth" "my_cluster" {
+  name = data.aws_eks_cluster.my_cluster.name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.my_cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.my_cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.my_cluster.token
+  # Removed the load_config_file argument
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  region             = "ap-south-1"
+  cluster_name       = "K8S-cluster"
+  availability_zones = slice(data.aws_availability_zones.available.names, 0, 3)
+}
+
+module "vpc" {
+  source               = "terraform-aws-modules/vpc/aws"
+  version              = "2.58.0"
+  name                 = "kas-vpc"
+  cidr                 = "172.16.0.0/16"
+  azs                  = local.availability_zones
+  private_subnets      = ["172.16.1.0/24", "172.16.2.0/24", "172.16.3.0/24"]
+  public_subnets       = ["172.16.4.0/24", "172.16.5.0/24", "172.16.6.0/24"]
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"            = "1"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                     = "1"
+  }
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "18.30.0"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.27"
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets  # Fixed: changed vpc_subnets to subnet_ids
+
+  eks_managed_node_groups = {
+    first = {
+      desired_capacity = 1
+      max_capacity     = 10
+      min_capacity     = 1
+      instance_type    = "t2.micro"
+    }
+  }
